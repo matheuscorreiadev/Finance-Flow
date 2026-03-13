8b0b3fbc63c811cc42479447dba820c9a217b87e
Matheus Correia <matheuscorreia1910@gmail.com>
2026-03-12 21:05:22 -0300

    Configurar projeto para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode }) => ({
-  base: "https://github.com/matheuscorreiadev/Finance-Flow", // 👈 caminho do repositório no GitHub Pages
-
-  server: {
-    host: "::",
-    port: 8080,
-  },
-
-  plugins: [
-    react(),
-    mode === "development" && componentTagger()
-  ].filter(Boolean),
-
+export default defineConfig({
+  base: "/Finance-Flow/", // 👈 Caminho relativo do repositório
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
